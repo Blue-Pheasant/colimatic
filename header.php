d8ccc3dad94797dc1156d888530a16e0de2bddc5
Võ Qúy Long <long10a102001@gmail.com>
2023-11-05 15:34:52 +0700

    Header

---
+++ header.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 
+<html <?php language_attributes(); ?> class="ie8">
+    <![endif]-->
+    <!--[if !IE]> 
+    <html <?php language_attributes(); ?>>
+        <![endif]-->
+        <head>
+            <meta charset="<?php bloginfo('charset'); ?>" />
+            <link rel="profile" href="http://gmpg.org/xfn/11" />
+            <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+            <?php wp_head(); ?>
+            <!-- This site is optimized with the Yoast SEO plugin v15.3 - https://yoast.com/wordpress/plugins/seo/ -->
+            <title>Machinery range - Colimatic</title>
+            <meta name="description"
+                content="Colimatic is specialized in the production of automatic packaging lines, filling units, traysealers, vacuum chamber machine, modified atmosphere.a" />
+            <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+            <link rel="canonical" href="https://www.colimatic.com/en/" />
+            <meta property="og:locale" content="en_US" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="Machinery range - Colimatic" />
+            <meta property="og:description"
+                content="Colimatic is specialized in the production of automatic packaging lines, filling units, traysealers, vacuum chamber machine, modified atmosphere.a" />
+            <meta property="og:url" content="https://www.colimatic.com/en/" />
+            <meta property="og:site_name" content="Colimatic" />
+            <meta property="article:modified_time" content="2023-05-19T15:13:31+00:00" />
+            <meta property="og:image" content="https://www.colimatic.com/wp-content/uploads/2019/04/alimentari.jpg" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:label1" content="Written by">
+            <meta name="twitter:data1" content="web@3dee.it">
+            <meta name="twitter:label2" content="Est. reading time">
+            <meta name="twitter:data2" content="1 minute">
+            <!-- / Yoast SEO plugin. -->
+            <link rel='stylesheet' id='generate-child-css'
+                href='https://www.colimatic.com/wp-content/themes/generatepress_child/style.css?ver=1636706405' media='all' />
+            
+            <script>
+                document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
+            </script>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="icon" href="https://www.colimatic.com/wp-content/uploads/2018/12/colimatic-favicon-150x150.png"
+                sizes="32x32" />
+            <link rel="icon" href="https://www.colimatic.com/wp-content/uploads/2018/12/colimatic-favicon-300x300.png"
+                sizes="192x192" />
+            <link rel="apple-touch-icon"
+                href="https://www.colimatic.com/wp-content/uploads/2018/12/colimatic-favicon-300x300.png" />
+            <meta name="msapplication-TileImage"
+                content="https://www.colimatic.com/wp-content/uploads/2018/12/colimatic-favicon-300x300.png" />
+        </head>
+        <body
+            class="home page-template page-template-elementor_header_footer page page-id-2932 wp-custom-logo wp-embed-responsive post-image-aligned-center secondary-nav-above-header secondary-nav-aligned-right sticky-menu-fade no-sidebar nav-float-right one-container fluid-header active-footer-widgets-4 header-aligned-left dropdown-hover elementor-default elementor-template-full-width elementor-kit-4215 elementor-page elementor-page-2932 full-width-content"
+            itemtype="https://schema.org/WebPage" itemscope>
+            <a class="screen-reader-text skip-link" href="#content" title="Skip to content">Skip to content</a>
+            <nav id="secondary-navigation" class="secondary-navigation" itemtype="https://schema.org/SiteNavigationElement"
+                itemscope="itemscope">
+                <div class="inside-navigation grid-container grid-parent">
+                    <button class="menu-toggle secondary-menu-toggle">
+                    <span class="mobile-menu">Menu</span>
+                    </button>
+                    <div class="main-nav">
+                        <ul id="menu-top-menu-inglese" class=" secondary-menu sf-menu">
+                            <li id="menu-item-2976"
+                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2976">
+                                <a target="_blank" rel="noopener noreferrer"
+                                    href="https://it-it.facebook.com/Coligroup/">Facebook
+                                </a>
+                            </li>
+                            <li id="menu-item-2977"
+                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2977">
+                                <a target="_blank" rel="noopener noreferrer"
+                                    href="https://it.linkedin.com/company/colimatic-packaging-machines---coligroup-spa">LinkedIn
+                                </a>
+                            </li>
+                            <li id="menu-item-2978"
+                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2978">
+                                <a target="_blank" rel="noopener noreferrer"
+                                    href="https://www.youtube.com/channel/UCYe1hvNtVbuO5CqFiGkAr6A">YouTube
+                                </a>
+                            </li>
+                            <li id="menu-item-wpml-ls-3-it"
+                                class="menu-item wpml-ls-slot-3 wpml-ls-item wpml-ls-item-it wpml-ls-menu-item wpml-ls-first-item menu-item-type-wpml_ls_menu_item menu-item-object-wpml_ls_menu_item menu-item-wpml-ls-3-it">
+                                <a title="Italian" href="https://www.colimatic.com">
+                                    <img alt=""
+                                        data-src="https://www.colimatic.com/wp-content/plugins/sitepress-multilingual-cms/res/flags/it.png"
+                                        class="wpml-ls-flag lazyload"
+                                        src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
+                                    <noscript>
+                                        <img class="wpml-ls-flag"
+                                            src="https://www.colimatic.com/wp-content/plugins/sitepress-multilingual-cms/res/flags/it.png"
+                                            alt="">
+                                    </noscript>
+                                    <span class="wpml-ls-display">Italian</span>
+                                </a>
+                            </li>
+                            <li id="menu-item-wpml-ls-3-en"
+                                class="menu-item wpml-ls-slot-3 wpml-ls-item wpml-ls-item-en wpml-ls-current-language wpml-ls-menu-item wpml-ls-last-item menu-item-type-wpml_ls_menu_item menu-item-object-wpml_ls_menu_item menu-item-wpml-ls-3-en">
+                                <a title="English" href="https://www.colimatic.com/en/">
+                                    <img alt=""
+                                        data-src="https://www.colimatic.com/wp-content/plugins/sitepress-multilingual-cms/res/flags/en.png"
+                                        class="wpml-ls-flag lazyload"
+                                        src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
+                                    <noscript>
+                                        <img class="wpml-ls-flag"
+                                            src="https://www.colimatic.com/wp-content/plugins/sitepress-multilingual-cms/res/flags/en.png"
+                                            alt="">
+                                    </noscript>
+                                    <span class="wpml-ls-display">English</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- .inside-navigation -->
+            </nav>
+            <!-- #secondary-navigation -->
+            <header id="masthead" class="site-header" itemtype="https://schema.org/WPHeader" itemscope>
+                <div class="inside-header grid-container grid-parent">
+                    <div class="site-logo">
+                        <a href="https://www.colimatic.com/en/" title="Colimatic" rel="home">
+                            <img alt="Colimatic" title="Colimatic"
+                                data-srcset="https://www.colimatic.com/wp-content/uploads/2020/11/1Logo-Colimatic-Coligroup-219x66-1.png 1x, https://www.colimatic.com/wp-content/uploads/2018/12/colimatic-retina-logo.png 2x"
+                                width="219" height="66"
+                                data-src="https://www.colimatic.com/wp-content/uploads/2020/11/1Logo-Colimatic-Coligroup-219x66-1.png"
+                                class="header-image is-logo-image lazyload"
+                                src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" />
+                            <noscript>
+                                <img class="header-image is-logo-image" alt="Colimatic"
+                                    src="https://www.colimatic.com/wp-content/uploads/2020/11/1Logo-Colimatic-Coligroup-219x66-1.png"
+                                    title="Colimatic"
+                                    srcset="https://www.colimatic.com/wp-content/uploads/2020/11/1Logo-Colimatic-Coligroup-219x66-1.png 1x, https://www.colimatic.com/wp-content/uploads/2018/12/colimatic-retina-logo.png 2x"
+                                    width="219" height="66" />
+                            </noscript>
+                        </a>
+                    </div>
+                    <nav id="site-navigation" class="main-navigation sub-menu-right"
+                        itemtype="https://schema.org/SiteNavigationElement" itemscope>
+                        <div class="inside-navigation">
+                            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+                            <span class="mobile-menu">Menu</span>
+                            </button>
+                            <div id="primary-menu" class="main-nav">
+                                <ul id="menu-main-menu-inglese" class=" menu sf-menu">
+                                    <li id="menu-item-2971" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-2932 current_page_item menu-item-2971">
+                                        <a href="<?php echo home_url(); ?>" aria-current="page">Home</a>
+                                    </li>
+                                    <li id="menu-item-2972" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2972">
+                                        <a href="<?php echo get_permalink(get_page_by_path('about-us')); ?>">About Us</a>
+                                    </li>
+                                    <li id="menu-item-2975" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2975">
+                                        <a href="<?php echo get_permalink(get_page_by_path('contact')); ?>">Contacts</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </header>
