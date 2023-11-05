f13c392ac9b312af02ccd19d7bcaad04856d6368
Võ Qúy Long <long10a102001@gmail.com>
2023-11-05 15:35:46 +0700

    Contact page

---
+++ templates/contact-template.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Template Name: Custom Contact Template
+*/
+get_header();
+?>
+
+<div class="page-hero">
+   <div class="inside-page-hero">
+      <div class="inside-page-hero__box">
+         <h1>Contacts</h1>
+      </div>
+   </div>
+</div>
+
+<div id="page" class="site grid-container container hfeed grid-parent">
+   <div id="content" class="site-content">
+      <div id="primary" class="content-area grid-parent mobile-grid-100 grid-100 tablet-grid-100">
+         <main id="main" class="site-main">
+            <article id="post-2951" class="post-2951 page type-page status-publish has-post-thumbnail" itemtype="https://schema.org/CreativeWork" itemscope="">
+               <div class="inside-article">
+                  <div class="entry-content" itemprop="text">
+                     <div class="wp-block-columns has-2-columns contact-section pill">
+                        <div class="wp-block-column pill__image"><iframe width="600" height="700" allowfullscreen="allowfullscreen" data-src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.8801083088174!2d9.913388215768512!3d45.53261813689627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47816bca49eeb59d%3A0x4087130ab07b4ad6!2sColigroup+spa!5e0!3m2!1sit!2sit!4v1555506523101!5m2!1sit!2sit" class=" lazyloaded" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="></iframe></div>
+                        <div class="wp-block-column pill__body background-color--white">
+                           <div class="contact-section__content">
+                              <figure>
+                                 <img data-src="https://www.colimatic.com/wp-content/uploads/2019/04/flag-ita-e1555514142549.jpg" class="contact-section__flag lazyloaded" src="https://www.colimatic.com/wp-content/uploads/2019/04/flag-ita-e1555514142549.jpg">
+                                 <noscript><img class="contact-section__flag" src="https://www.colimatic.com/wp-content/uploads/2019/04/flag-ita-e1555514142549.jpg"/></noscript>
+                              </figure>
+                              <div class="contact-section__text">
+                                 <p><strong>Coligroup SPA</strong> <br>via del Lavoro A. 9<br>25032 Chiari (Brescia) Italy<br>Phone: +39 030 7000761/2/3<br>Fax: +39 030 713370
+                                    <br>
+                                    e-mail:&nbsp;<a href="mailto:info@colimatic.com">info@colimatic.com</a>
+                                    <br>
+                                 </p>
+                                 <hr>
+                                 <b>Spare Parts dept:</b>&nbsp;<a href="mailto:spare@colimatic.com">spare@colimatic.com</a><br>
+                                 <b>After-sales service:</b>&nbsp;<a href="mailto:tech@colimatic.com">tech@colimatic.com</a>
+                                 <br>
+                                 <b>Sale dept:</b>&nbsp;<a href="mailto:backoffice@colimatic.com">backoffice@colimatic.com</a>
+                                 <br>
+                                 <b>Book-keeping dept:</b>&nbsp;<a href="mailto:amministrazione@colimatic.com">amministrazione@colimatic.com</a>
+                                 <p></p>
+                              </div>
+                           </div>
+                           <div class="contact-section__content">
+                              <img data-src="https://www.colimatic.com/wp-content/uploads/2019/04/flag-usa-e1555514175956.jpg" class="contact-section__flag lazyloaded" src="https://www.colimatic.com/wp-content/uploads/2019/04/flag-usa-e1555514175956.jpg">
+                              <noscript><img class="contact-section__flag" src="https://www.colimatic.com/wp-content/uploads/2019/04/flag-usa-e1555514175956.jpg"/></noscript>
+                              <div class="contact-section__text">
+                                 <p><strong>Colimatic USA Inc</strong><br>9272 Jeronimo Road , Suite 115<br>Irvine, CA 92618 USA<br>Phone:+1 -949-600-6440<br>Fax:+1 -949-600-6441<br>e-mail:&nbsp;<a href="mailto:info@colimaticusa.com">info@colimaticusa.com</a><br>web:&nbsp;<a rel="noreferrer noopener" href="http://www.colimaticusa.com/" target="_blank">www.colimaticusa.com</a> </p>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="social-widget"><a href="https://it-it.facebook.com/Coligroup/" target="_blank" rel="noopener noreferrer"><span class="fa-stack fa-lg">
+                        <i class="fa fa-circle fa-stack-2x icon-background1"></i>
+                        <i class="fab fa-facebook-f fa-stack-1x"></i>
+                        </span>
+                        </a>
+                        <a href="https://it.linkedin.com/company/colimatic-packaging-machines---coligroup-spa" target="_blank" rel="noopener noreferrer">
+                        <span class="fa-stack fa-lg">
+                        <i class="fa fa-circle fa-stack-2x icon-background1"></i>
+                        <i class="fab fa-linkedin-in fa-stack-1x"></i>
+                        </span>
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCYe1hvNtVbuO5CqFiGkAr6A" target="_blank" rel="noopener noreferrer">
+                        <span class="fa-stack fa-lg">
+                        <i class="fa fa-circle fa-stack-2x icon-background1"></i>
+                        <i class="fab fa-youtube fa-stack-1x"></i>
+                        </span>
+                        </a>
+                     </div>
+                     <p></p>
+                  </div>
+               </div>
+            </article>
+         </main>
+      </div>
+   </div>
+</div>
+
+<?php get_footer(); ?>
