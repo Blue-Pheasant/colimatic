9444d6feeaba43c504d22af3e9bd98e38410ffbb
Võ Qúy Long <long10a102001@gmail.com>
2023-11-11 17:12:52 +0700

    Update footer

--- footer.php
+++ footer.php
@@ -296,6 +296,91 @@
 ?>
 <script src="<?php echo $script_url; ?>"></script>
 
+<script id="elementor-pro-frontend-js-before">
+   var ElementorProFrontendConfig = {
+       "ajaxurl": "https:\/\/www.colimatic.com\/wp-admin\/admin-ajax.php",
+       "nonce": "f4c6608de8",
+       "i18n": {
+           "toc_no_headings_found": "No headings were found on this page."
+       },
+       "shareButtonsNetworks": {
+           "facebook": {
+               "title": "Facebook",
+               "has_counter": true
+           },
+           "twitter": {
+               "title": "Twitter"
+           },
+           "google": {
+               "title": "Google+",
+               "has_counter": true
+           },
+           "linkedin": {
+               "title": "LinkedIn",
+               "has_counter": true
+           },
+           "pinterest": {
+               "title": "Pinterest",
+               "has_counter": true
+           },
+           "reddit": {
+               "title": "Reddit",
+               "has_counter": true
+           },
+           "vk": {
+               "title": "VK",
+               "has_counter": true
+           },
+           "odnoklassniki": {
+               "title": "OK",
+               "has_counter": true
+           },
+           "tumblr": {
+               "title": "Tumblr"
+           },
+           "digg": {
+               "title": "Digg"
+           },
+           "skype": {
+               "title": "Skype"
+           },
+           "stumbleupon": {
+               "title": "StumbleUpon",
+               "has_counter": true
+           },
+           "mix": {
+               "title": "Mix"
+           },
+           "telegram": {
+               "title": "Telegram"
+           },
+           "pocket": {
+               "title": "Pocket",
+               "has_counter": true
+           },
+           "xing": {
+               "title": "XING",
+               "has_counter": true
+           },
+           "whatsapp": {
+               "title": "WhatsApp"
+           },
+           "email": {
+               "title": "Email"
+           },
+           "print": {
+               "title": "Print"
+           }
+       },
+       "facebook_sdk": {
+           "lang": "en_US",
+           "app_id": ""
+       },
+       "lottie": {
+           "defaultAnimationUrl": "https:\/\/www.colimatic.com\/wp-content\/plugins\/elementor-pro\/modules\/lottie\/assets\/animations\/default.json"
+       }
+   };
+</script>
 <?php wp_footer(); ?>
 </body> <!-- end body -->
 
