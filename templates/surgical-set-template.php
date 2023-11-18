037f74ea345a01618d91adf1b91c1c5eada691f6
Võ Qúy Long <long10a102001@gmail.com>
2023-11-18 12:12:45 +0700

    Surgical set template

---
+++ templates/surgical-set-template.php
@@ -0,0 +1,131 @@
+<?php
+/*
+Template Name: Custom Surgical Set Template
+*/
+get_header();
+?>
+
+<div id="page" class="site grid-container container hfeed grid-parent">
+   <div id="content" class="site-content">
+      <div id="primary" class="content-area grid-parent mobile-grid-100 grid-100 tablet-grid-100">
+         <main id="main" class="site-main">
+            <div class="inside-article">
+               <div class="entry-content space--top-0" itemprop="text">
+                  <div class="breadcrumb">
+                     <span><span><a href="https://www.colimatic.com/en/machinery-range/">machinery range</a> &gt; <span><a href="https://www.colimatic.com/en/machinery-range/medical-line-automation/">Medical Line Automation</a> &gt; <span class="breadcrumb_last" aria-current="page">SURGICAL SET</span></span></span></span>					
+                  </div>
+                  <div class="featured-image">
+                     <a data-fancybox="SURGICAL SET" href="https://www.colimatic.com/wp-content/uploads/2019/04/surgical.png">
+                        <img width="2000" height="1125" alt="Termoformatrice Surgical Set - Colimatic termoformatrici industriali" data-srcset="https://www.colimatic.com/wp-content/uploads/2019/04/surgical.png 2000w, https://www.colimatic.com/wp-content/uploads/2019/04/surgical-300x169.png 300w, https://www.colimatic.com/wp-content/uploads/2019/04/surgical-768x432.png 768w, https://www.colimatic.com/wp-content/uploads/2019/04/surgical-1024x576.png 1024w" data-src="https://www.colimatic.com/wp-content/uploads/2019/04/surgical.png" data-sizes="(max-width: 2000px) 100vw, 2000px" class="attachment-post-thumbnail size-post-thumbnail wp-post-image lazyloaded" src="https://www.colimatic.com/wp-content/uploads/2019/04/surgical.png" sizes="(max-width: 2000px) 100vw, 2000px" srcset="https://www.colimatic.com/wp-content/uploads/2019/04/surgical.png 2000w, https://www.colimatic.com/wp-content/uploads/2019/04/surgical-300x169.png 300w, https://www.colimatic.com/wp-content/uploads/2019/04/surgical-768x432.png 768w, https://www.colimatic.com/wp-content/uploads/2019/04/surgical-1024x576.png 1024w">
+                        <noscript><img width="2000" height="1125" src="https://www.colimatic.com/wp-content/uploads/2019/04/surgical.png" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="Termoformatrice Surgical Set - Colimatic termoformatrici industriali" srcset="https://www.colimatic.com/wp-content/uploads/2019/04/surgical.png 2000w, https://www.colimatic.com/wp-content/uploads/2019/04/surgical-300x169.png 300w, https://www.colimatic.com/wp-content/uploads/2019/04/surgical-768x432.png 768w, https://www.colimatic.com/wp-content/uploads/2019/04/surgical-1024x576.png 1024w" sizes="(max-width: 2000px) 100vw, 2000px" /></noscript>
+                     </a>
+                  </div>
+                  <div class="background-color--white alignfull">
+                     <div class="grid-container grid-parent">
+                        <div class="wrap">
+                           <div class="intro">
+                              <div class="intro__body">
+                                 <div class="intro__title">
+                                    <h1>SURGICAL SET</h1>
+                                 </div>
+                                 <div class="intro__content"></div>
+                              </div>
+                           </div>
+                           <div class="wp-content padding--both-0">
+                           </div>
+                           <div class="spec">
+                           </div>
+                           <!-- .spec -->
+                           <div class="request-form" id="request-form">
+                              <h3>request informations</h3>
+                              <div role="form" class="wpcf7" id="wpcf7-f1998-p3032-o1" lang="it-IT" dir="ltr">
+                                 <div class="screen-reader-response">
+                                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                                    <ul></ul>
+                                 </div>
+                                 <form action="/en/machinery-range/medical-line-automation/thermoforming-surgical-set/#wpcf7-f1998-p3032-o1" method="post" class="wpcf7-form init" novalidate="novalidate" data-status="init">
+                                    <div style="display: none;">
+                                       <input type="hidden" name="_wpcf7" value="1998">
+                                       <input type="hidden" name="_wpcf7_version" value="5.3.2">
+                                       <input type="hidden" name="_wpcf7_locale" value="it_IT">
+                                       <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f1998-p3032-o1">
+                                       <input type="hidden" name="_wpcf7_container_post" value="3032">
+                                       <input type="hidden" name="_wpcf7_posted_data_hash" value="">
+                                       <input type="hidden" name="_wpcf7_recaptcha_response" value="03AFcWeA4wshNaPm5Oh0eZqbMA-zTEajlp5Z7yOhYk9TB3ClG1aDXkkhZB20joa07gDftpOPYiAajK7Hhcyev1lwXmDL7a3Y4LO7CPdGExyxqUioVWdj9Ko79nS0f03vS2zRX0MmGDsDENUE93C7avvyhX5v37Vc_PAAAK3g0jt6W1quuno61HINptSUrt31KNNov0rCmd1hecZiGVCzSrQSwigjTE8hfh8CDEe6hj6EHT5rd8H7XOE8mqyIeu8vdW8S6q5tYCWn_pASMlUPkUNR17ECUM4YI81HmOccsiJjJzbuqisERPJxj2pNdBLPfimN8BrvojXrrNEY_UAYkOFFylfcm6M8O1UWTGvzCCf9Pihb7gBvHc0rOkJr3CFgGJwZAhrL_HZavhtwDsVK9fmYLn8Qwx6dhGfNd6DRuJGsxrqvJFWCjOIbvyzBRpRsy0OInNLMyvLYmJSCO8bOBFTTJ1RwMVcYYgQUghsDomfljoCYw0uOx1P1DtgK-573BU78JFM52bfrCL8iixfXXi2ucz5hNL_wTzhFI49FmnhT4WJ6lk2QWChOIVAuaLUDu5J-3J1okzFC37s78a5UOBz1-xZwYnMUQDRw">
+                                    </div>
+                                    <div class="cf">
+                                       <div class="cf-name"> Nominativo *<br>
+                                          <span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span>
+                                       </div>
+                                       <div class="cf-company">
+                                          Azienda *<br>
+                                          <span class="wpcf7-form-control-wrap your-company"><input type="text" name="your-company" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false"></span>
+                                       </div>
+                                       <div class="cf-email">
+                                          E-Mail *<br>
+                                          <span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false"></span>
+                                       </div>
+                                       <div class="cf-phone">
+                                          Recapito telefonico *<br>
+                                          <span class="wpcf7-form-control-wrap your-phone"><input type="text" name="your-phone" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span>
+                                       </div>
+                                       <div class="cf-message">
+                                          Messaggio<br>
+                                          <span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false"></textarea></span>
+                                       </div>
+                                       <div class="cf-privacy">
+                                          <span class="wpcf7-form-control-wrap privacy-policy"><span class="wpcf7-form-control wpcf7-acceptance"><span class="wpcf7-list-item"><label><input type="checkbox" name="privacy-policy" value="1" aria-invalid="false"><span class="wpcf7-list-item-label">Dichiaro di avere letto ed accettato la <a href="/privacy-policy" target="_blank" alt="Privacy Policy">Privacy Policy</a></span></label></span></span></span>
+                                       </div>
+                                       <div class="cf-submit">
+                                          <input type="submit" value="Invia" class="wpcf7-form-control wpcf7-submit" disabled=""><span class="ajax-loader"></span>
+                                       </div>
+                                    </div>
+                                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                                 </form>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </main>
+         <!-- #main -->
+      </div>
+      <!-- #primary -->
+      <div id="side-links" class="st-right">
+         <div class="st-btn">
+            <a href="https://www.colimatic.com/wp-content/uploads/2019/05/Linea_Automation_Medicale_ENG.pdf" target="_blank">
+               <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Livello_1" x="0px" y="0px" viewBox="0 0 45 58" style="enable-background:new 0 0 45 58;" xml:space="preserve" class="svg replaced-svg">
+                  <g id="pdf" transform="translate(-6.5)">
+                     <path id="Path_21854" d="M51,12.2l-0.8-0.8L40.1,1.3l-0.8-0.8C39,0.2,38.5,0,38,0H9C7.8,0,6.5,0.9,6.5,2.9V56   c0.1,1,0.9,1.7,1.8,1.9c0.1,0,0.1,0,0.1,0c0.2,0,0.3,0,0.5,0H49c0.2,0,0.3,0,0.5,0c0.1,0,0.1,0,0.2,0c1-0.2,1.7-0.9,1.8-1.9V14   C51.6,13.3,51.4,12.7,51,12.2z M47.9,12h-8.4V3.6L47.9,12z M9,56.2c-0.1,0-0.4,0-0.4,0c-0.2-0.1-0.3-0.2-0.3-0.4V41l41.5,0.1v14.7   c0,0.2-0.1,0.3-0.3,0.4c-0.1,0-0.4-0.1-0.5-0.1L9,56.2L9,56.2z M8.3,39V2.9c0-0.2,0-1.2,0.5-1.2l29.1-0.1c0,0.1-0.1,0.6-0.1,0.7   l-0.1,11.5L49,13.9c0.1,0,0.6,0.1,0.7,0l-0.3-0.1L49.8,39L8.5,38.9L8.3,39z"></path>
+                     <path id="Path_21855" d="M22,44.7c-0.3-0.3-0.7-0.5-1.1-0.6s-0.8-0.2-1.3-0.2h-2.9V54h1.6v-3.6h1.2c0.5,0,1-0.1,1.4-0.2   c0.4-0.1,0.8-0.4,1.1-0.7c0.3-0.3,0.6-0.6,0.7-1c0.2-0.4,0.3-0.9,0.3-1.4s-0.1-0.9-0.3-1.3C22.6,45.4,22.4,45,22,44.7z M21.4,48.1   c-0.1,0.2-0.2,0.5-0.4,0.6c-0.2,0.1-0.3,0.3-0.5,0.3c-0.2,0.1-0.4,0.1-0.6,0.1h-1.5v-4h1.2c0.3,0,0.7,0.1,1,0.2   c0.2,0.1,0.4,0.3,0.6,0.5c0.1,0.2,0.2,0.4,0.3,0.6c0,0.2,0.1,0.4,0.1,0.6C21.6,47.4,21.5,47.7,21.4,48.1z"></path>
+                     <path id="Path_21856" d="M32,45.4c-0.5-0.5-1-0.8-1.6-1.1c-0.7-0.3-1.5-0.4-2.2-0.4h-3V54H29c0.2,0,0.4,0,0.6,0   c0.3,0,0.6-0.1,0.9-0.2c0.3-0.1,0.7-0.3,1-0.5c0.4-0.3,0.7-0.6,0.9-0.9c0.3-0.5,0.5-1,0.7-1.5c0.2-0.7,0.3-1.4,0.3-2.1   c0-0.6-0.1-1.2-0.3-1.8C32.7,46.4,32.4,45.8,32,45.4z M30.8,51.7c-0.6,0.7-1.5,1.2-2.4,1.1h-1.6v-7.6h1c0.7,0,1.3,0.1,1.9,0.3   c0.4,0.2,0.8,0.5,1.2,0.8c0.3,0.3,0.5,0.7,0.6,1.1c0.1,0.4,0.1,0.8,0.1,1.2C31.6,49.7,31.3,50.8,30.8,51.7L30.8,51.7z"></path>
+                     <path id="Path_21857" d="M35.6,54h1.7v-4.5h4.2v-1.1h-4.2v-3.2h4.6V44h-6.3V54z"></path>
+                     <path id="Path_21858" d="M38.4,23c-1.1,0-2.3,0.1-3.4,0.4c-2-2.3-3.8-4.8-5.1-7.6c1.3-5.6,0.7-6.4,0.4-6.8C30,8.6,29.5,8,29,8   c-0.3,0-0.7,0.1-1,0.2c-0.5,0.2-1,0.7-1.1,1.2c-0.7,1.7,0.3,4.5,1.3,6.6c-1,3.7-2.2,7.3-3.8,10.8c-3.8,1.7-5.8,3.4-6,5.1   c-0.1,0.9,0.4,1.8,1.1,2.2c0.3,0.2,0.6,0.3,1,0.3l0,0c0.9,0,1.8-0.7,2.8-2.1c0.9-1.3,1.7-2.8,2.4-4.3c2.8-1.2,5.8-2.2,8.8-2.8   c1.6,1.5,3,2.3,4.2,2.3c0.9,0,1.7-0.4,2.2-1.2c0.5-0.6,0.7-1.4,0.4-2.2C40.9,23.3,40,23,38.4,23z M20.5,32.6   c-0.5-0.4-0.4-0.6-0.4-0.7c0.1-0.6,0.9-1.5,3.1-2.7C21.6,32.2,20.7,32.6,20.5,32.6z M28.7,9.7c0,0,1,1.1,0.1,3.2   C28,12,27.9,10.7,28.7,9.7z M26.7,25.7c1-2.5,1.9-5,2.7-7.6c1.1,2,2.4,3.8,3.8,5.5C31,24.2,28.8,24.9,26.7,25.7z M39.6,25.3   c-0.3,0.3-0.8,0.5-1.2,0.4c-0.5,0-0.7-0.3-1.5-0.9c0.6-0.1,1.2-0.1,1.8-0.1c0.9,0,1.1,0.1,1.2,0.2C39.7,25,39.7,25.1,39.6,25.3   L39.6,25.3z"></path>
+                  </g>
+               </svg>
+               <span class="st-label">Catalogue</span>
+            </a>
+         </div>
+         <div class="st-btn">
+            <a href="#request-form" class="smooth-scroll">
+               <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Livello_1" x="0px" y="0px" viewBox="0 0 52.7 37.5" style="enable-background:new 0 0 52.7 37.5;" xml:space="preserve" class="svg replaced-svg">
+                  <g id="mail" transform="translate(0 -4.482)">
+                     <g id="Group_1187" transform="translate(9.185 22.556)">
+                        <path id="Path_21861" d="M33.5,10.9c-0.2,0-0.4-0.1-0.6-0.2l-9.8-9.2c-0.3-0.3-0.4-0.9,0-1.2c0.3-0.3,0.9-0.4,1.2,0c0,0,0,0,0,0    l9.8,9.2c0.3,0.3,0.4,0.9,0,1.2C34,10.8,33.7,10.9,33.5,10.9z"></path>
+                        <path id="Path_21862" d="M0.9,10.9C0.4,10.9,0,10.5,0,10c0-0.2,0.1-0.5,0.3-0.6l9.8-9.2c0.3-0.3,0.9-0.3,1.2,0s0.3,0.9,0,1.2    c0,0,0,0,0,0l-9.8,9.2C1.3,10.8,1.1,10.9,0.9,10.9z"></path>
+                     </g>
+                     <path id="Path_21863" d="M48.5,42H4.3C1.9,42,0,40.1,0,37.7v-29c0-2.3,1.9-4.2,4.3-4.3h44.2c2.3,0,4.2,1.9,4.3,4.3v29   C52.7,40.1,50.8,42,48.5,42z M4.3,6.2c-1.4,0-2.5,1.1-2.6,2.6v29c0,1.4,1.1,2.5,2.6,2.6h44.2c1.4,0,2.5-1.1,2.6-2.6v-29   c0-1.4-1.1-2.5-2.6-2.6L4.3,6.2z"></path>
+                     <path id="Path_21864" d="M26.4,27.5c-1.1,0-2.3-0.4-3.1-1.1L1.4,7.3C1.1,7,1,6.5,1.3,6.1S2.2,5.7,2.5,6l0,0l21.8,19   c1.2,0.9,2.8,0.9,4,0l21.8-19c0.4-0.3,0.9-0.3,1.2,0.1s0.3,0.9-0.1,1.2l-21.8,19C28.6,27.1,27.5,27.5,26.4,27.5z"></path>
+                  </g>
+               </svg>
+               <span class="st-label">Quote</span>
+            </a>
+         </div>
+      </div>
+      <!-- #side-links -->
+   </div>
+</div>
+
+<?php get_footer(); ?>
